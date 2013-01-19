b1bba6c8515823abda28555a38bd9886efeb22b0
Erich Schroeter <erichschroeter@gmail.com>
2013-01-18 18:42:01 -0600

    Enabled init command.

--- src/commands/Command.c
+++ src/commands/Command.c
@@ -1,6 +1,8 @@
 #include "Command.h"
+#include "InitCommand.h"
 
 struct command command_table[] = {
+	{"init", initCmd_help, initCmd_parse, initCmd_execute},
 };
 const size_t command_table_size = sizeof(command_table) / sizeof(command_table[0]);
 
