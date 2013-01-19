8e681d6305672bae055c63ba0106c9f8dc473ead
Erich Schroeter <erichschroeter@gmail.com>
2013-01-18 21:13:25 -0600

    Enabled help command.

--- src/commands/Command.c
+++ src/commands/Command.c
@@ -1,8 +1,10 @@
 #include "Command.h"
 #include "InitCommand.h"
+#include "HelpCommand.h"
 
 struct command command_table[] = {
 	{"init", initCmd_help, initCmd_parse, initCmd_execute},
+	{"help", helpCmd_help, helpCmd_parse, helpCmd_execute},
 };
 const size_t command_table_size = sizeof(command_table) / sizeof(command_table[0]);
 
