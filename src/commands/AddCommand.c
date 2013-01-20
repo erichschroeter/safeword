eb1719d6bf7b4ee056afb07efb0ea14f000ce39f
Erich Schroeter <erichschroeter@gmail.com>
2013-01-20 10:52:14 -0600

    Implemented --message option to store description in credentials table. Temporarily removed insert statements for username and password.

--- src/commands/AddCommand.c
+++ src/commands/AddCommand.c
@@ -9,34 +9,52 @@
 
 static char* username = NULL;
 static char* password = NULL;
+static char* description = NULL;
 
 char* addCmd_help(void)
 {
 	return "SYNOPSIS\n"
-"	add USERNAME PASSWORD\n"
+"	add [-m | --message MESSAGE] USERNAME PASSWORD\n"
 "\n";
 }
 
 int addCmd_parse(int argc, char** argv)
 {
-	int ret = 0;
+	int ret = 0, c;
+	struct option long_options[] = {
+		{"message",	required_argument,	NULL,	'm'},
+	};
 
-	if (argc < 3)
+	while ((c = getopt_long(argc, argv, "m:", long_options, 0)) != -1) {
+		switch (c) {
+		case 'm':
+			description = calloc(strlen(optarg), sizeof(char));
+			if (!description) {
+				ret = -ENOMEM;
+				goto fail;
+			}
+			description = strcpy(description, optarg);
+			break;
+		}
+	}
+
+	if ((argc - optind) < 2)
 		return ret;
 
-	username = calloc(strlen(argv[1]), sizeof(char));
+	username = calloc(strlen(argv[optind]), sizeof(char));
 	if (!username) {
 		ret = -ENOMEM;
 		goto fail;
 	}
-	username = strcpy(username, argv[1]);
+	username = strcpy(username, argv[optind]);
+	optind++;
 
-	password = calloc(strlen(argv[2]), sizeof(char));
+	password = calloc(strlen(argv[optind]), sizeof(char));
 	if (!password) {
 		ret = -ENOMEM;
 		goto fail_password;
 	}
-	password = strcpy(password, argv[2]);
+	password = strcpy(password, argv[optind]);
 
 	return ret;
 
@@ -50,6 +68,7 @@ int addCmd_execute(void)
 {
 	int ret;
 	sqlite3* handle;
+	sqlite3_int64 credentials_rowid;
 	char* sql;
 
 	if (!username || !password)
@@ -61,28 +80,30 @@ int addCmd_execute(void)
 		return -ret;
 	}
 
-	sql = calloc(strlen(username) + 100, sizeof(char));
-	if (!sql) {
-		ret = -ENOMEM;
-		goto fail;
-	}
-
-	sprintf(sql, "INSERT INTO usernames (username) VALUES ('%s')", username);
+	sql = calloc(100, sizeof(char));
+	sprintf(sql, "INSERT INTO credentials DEFAULT VALUES");
 	ret = sqlite3_exec(handle, sql, 0, 0, 0);
 	free(sql);
+	credentials_rowid = sqlite3_last_insert_rowid(handle);
+	if (description) {
+		sql = calloc(strlen(description) + 100, sizeof(char));
+		if (!sql) {
+			ret = -ENOMEM;
+			goto fail;
+		}
 
-	sql = calloc(strlen(password) + 100, sizeof(char));
-	if (!sql) {
-		ret = -ENOMEM;
-		goto fail;
+		sprintf(sql, "UPDATE credentials SET description = '%s' WHERE "
+			"id = %d",
+			description, credentials_rowid);
+		ret = sqlite3_exec(handle, sql, 0, 0, 0);
+		free(sql);
 	}
-
-	sprintf(sql, "INSERT INTO passwords (password) VALUES ('%s')", password);
-	ret = sqlite3_exec(handle, sql, 0, 0, 0);
 
 	sqlite3_close(handle);
 
 fail:
-	free(sql);
+	free(username);
+	free(password);
+	free(description);
 	return ret;
 }
