a4a2e7550b5919aed321b32c1fca236640fab963
Erich Schroeter <erichschroeter@gmail.com>
2013-01-26 16:57:52 -0600

    Enabled tag command.

--- src/commands/Command.c
+++ src/commands/Command.c
@@ -3,12 +3,14 @@
 #include "HelpCommand.h"
 #include "AddCommand.h"
 #include "ListCommand.h"
+#include "TagCommand.h"
 
 struct command command_table[] = {
 	{"init", initCmd_help, initCmd_parse, initCmd_execute},
 	{"help", helpCmd_help, helpCmd_parse, helpCmd_execute},
 	{"add", addCmd_help, addCmd_parse, addCmd_execute},
 	{"ls", listCmd_help, listCmd_parse, listCmd_execute},
+	{"tag", tagCmd_help, tagCmd_parse, tagCmd_execute},
 };
 const size_t command_table_size = sizeof(command_table) / sizeof(command_table[0]);
 
