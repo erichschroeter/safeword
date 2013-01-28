6d847c886dcc35e7dbda2001dbd0d1f48829cdb6
Erich Schroeter <erichschroeter@gmail.com>
2013-01-28 17:01:16 -0600

    Implemented -v | --version option to print the version.

--- src/safeword.c
+++ src/safeword.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "safeword.h"
 #include "commands/Command.h"
 
 void print_usage()
 {
 	printf("safeword COMMAND [OPTIONS] [ARGS]\n");
+}
+
+void print_version()
+{
+	printf("%d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 }
 
 void print_supported_commands()
@@ -33,16 +39,32 @@ void print_possible_commands(const char* command_str)
 int main(int argc, char** argv)
 {
 	const char* command_str;
-	int res = 0;
+	int res = 0, i, command_index = 1;
 
-	// minimum of 2 parameters (safeword command itself and a subcommand)
+	/* process any options before subcommand */
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			if (!strcmp("-v", argv[i]) || !strcmp("--version", argv[i])) {
+				print_version();
+				return 0;
+			} else {
+				fprintf(stderr, "unknown option '%s'\n", argv[i]);
+				return 1;
+			}
+			command_index++;
+		} else {
+			break;
+		}
+	}
+
+	/* minimum of 2 parameters (safeword command itself and a subcommand) */
 	if (argc < 2) {
 		print_usage();
 		print_supported_commands();
 		return 0;
 	}
 
-	command_str = argv[1];
+	command_str = argv[command_index];
 
 	if (command_str) {
 		int i, matches = 0, command_index = -1;
