b6f988693aefcc82f20b19bf23c2a53a63554609
Erich Schroeter <erichschroeter@gmail.com>
2013-02-02 13:42:52 -0600

    Created safeword_db_open() to handle opening the db connection and handle error checking; this reduces code throughout the commands.

--- src/safeword.c
+++ src/safeword.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "safeword.h"
 #include "commands/Command.h"
+
+int safeword_db_open(sqlite3 **handle)
+{
+	int ret = 0;
+	char *db = malloc(sizeof(char*));
+
+	if (!db) {
+		ret = -ENOMEM;
+		goto fail;
+	}
+	db = getenv("SAFEWORD_DB");
+	if (!db) {
+		ret = -ESAFEWORD_ENV_NOT_SET;
+		fprintf(stderr, "SAFEWORD_DB environment variable not set\n");
+		goto fail;
+	}
+
+	// TODO check if db is a file and exists
+
+	ret = sqlite3_open(db, handle);
+	if (ret) {
+		fprintf(stderr, "failed to open safeword database '%s'\n", db);
+		goto fail;
+	}
+
+fail:
+	return ret;
+}
 
 void print_usage()
 {
