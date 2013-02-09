65e3fa081302624b3b068d6bd9c17175101a77cb
Erich Schroeter <erichschroeter@gmail.com>
2013-02-09 11:49:41 -0600

    Initial implementation of --delete and --force options. User is prompted to delete each tag if --force option is not present.

--- src/commands/TagCommand.c
+++ src/commands/TagCommand.c
@@ -8,6 +8,11 @@
 #include <safeword.h>
 #include "TagCommand.h"
 
+#define MAXBUFFERSIZE 16
+
+static int force;
+static int delete;
+
 static int *credential_ids;
 static int credential_ids_size;
 static char **tags;
@@ -31,75 +36,42 @@ char* tagCmd_help(void)
 "\n";
 }
 
-int tagCmd_parse(int argc, char** argv)
+static int tag_callback(void* not_used, int argc, char** argv, char** col_name)
 {
-	int ret = 0, remaining_args = 0, c, backup;
-	struct option long_options[] = {
-		{"delete",	no_argument,	NULL,	'd'},
-		{"force",	no_argument,	NULL,	'f'},
-	};
+	tag_id = atoi(argv[0]);
+	return 0;
+}
 
-	while ((c = getopt_long(argc, argv, "df", long_options, 0)) != -1) {
-		switch (c) {
-		case 'd':
-			break;
-		case 'f':
-			break;
-		}
+static int delete_tag_callback(void* not_used, int argc, char** argv, char** col_name)
+{
+	tag_id = atoi(argv[0]);
+	return 0;
+}
+
+static int print_tag_callback(void* not_used, int argc, char** argv, char** col_name)
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
+
+static int delete_tag(sqlite3* handle, const char* tag)
+{
+	int ret;
+	char *sql;
+
+	sql = calloc(strlen(tag) + 100, sizeof(char));
+	if (!sql) {
+		ret = -ENOMEM;
+		goto fail;
 	}
-
-	remaining_args = argc - optind;
-	backup = optind;
-	if (remaining_args > 1) {
-		int i = 0, id;
-		char *id_str;
-		char *ids_backup = calloc(strlen(argv[optind]), sizeof(char));
-		tags_size = 0;
-		tags = malloc(remaining_args * sizeof(*tags));
-		strcpy(ids_backup, argv[optind]);
-
-		/* get number of credential ids in string */
-		id_str = strtok(ids_backup, ",");
-		while (id_str != NULL) {
-			id_str = strtok(NULL, ",");
-			credential_ids_size++;
-		}
-
-		credential_ids = calloc(credential_ids_size, sizeof(*credential_ids));
-		ids_backup = argv[optind];
-		id_str = strtok(ids_backup, ",");
-		while (id_str != NULL) {
-			id = atoi(id_str);
-			credential_ids[i] = id;
-			id_str = strtok(NULL, ",");
-			i++;
-		}
-		optind++;
-
-		for (i = 0; i < remaining_args - 1; i++) {
-			tags[i] = calloc(strlen(argv[optind]), sizeof(char));
-			if (!tags[i]) {
-				ret = -ENOMEM;
-				goto fail;
-			}
-			strcpy(tags[i], argv[optind]);
-			tags_size++;
-			optind++;
-		}
-		optind = backup;
-	}
+	sprintf(sql, "DELETE FROM tags WHERE tag='%s';", tag);
+	ret = sqlite3_exec(handle, sql, delete_tag_callback, 0, 0);
+	free(sql);
 fail:
 	return ret;
 }
 
-static int tag_callback(void* not_used, int argc, char** argv, char** col_name)
-{
-	tag_id = atoi(argv[0]);
-
-	return 0;
-}
-
-int map_tag(sqlite3* handle, const char* tag, sqlite3_int64 credential_id)
+static int map_tag(sqlite3* handle, const char* tag, sqlite3_int64 credential_id)
 {
 	int ret;
 	char *sql;
@@ -142,23 +114,130 @@ fail:
 	return ret;
 }
 
-static int print_tag_callback(void* not_used, int argc, char** argv, char** col_name)
+int tagCmd_parse(int argc, char** argv)
 {
-	printf("%s\n", argv[0]);
-	return 0;
+	int ret = 0, remaining_args = 0, c, backup;
+	struct option long_options[] = {
+		{"delete",	no_argument,	NULL,	'd'},
+		{"force",	no_argument,	NULL,	'f'},
+	};
+
+	while ((c = getopt_long(argc, argv, "df", long_options, 0)) != -1) {
+		switch (c) {
+		case 'd':
+			delete = 1;
+			break;
+		case 'f':
+			force = 1;
+			break;
+		}
+	}
+
+	remaining_args = argc - optind;
+	backup = optind;
+	if (remaining_args > 1 && !delete) {
+		int i = 0, id;
+		char *id_str;
+		char *ids_backup = calloc(strlen(argv[optind]), sizeof(char));
+		tags_size = 0;
+		tags = malloc(remaining_args * sizeof(*tags));
+		if (!tags) {
+			ret = -ENOMEM;
+			goto fail;
+		}
+		strcpy(ids_backup, argv[optind]);
+
+		/* get number of credential ids in string */
+		id_str = strtok(ids_backup, ",");
+		while (id_str != NULL) {
+			id_str = strtok(NULL, ",");
+			credential_ids_size++;
+		}
+
+		credential_ids = calloc(credential_ids_size, sizeof(*credential_ids));
+		if (!credential_ids) {
+			ret = -ENOMEM;
+			goto fail;
+		}
+		ids_backup = argv[optind];
+		id_str = strtok(ids_backup, ",");
+		while (id_str != NULL) {
+			id = atoi(id_str);
+			credential_ids[i] = id;
+			id_str = strtok(NULL, ",");
+			i++;
+		}
+		optind++;
+
+		for (i = 0; i < remaining_args - 1; i++) {
+			tags[i] = calloc(strlen(argv[optind]), sizeof(char));
+			if (!tags[i]) {
+				ret = -ENOMEM;
+				goto fail;
+			}
+			strcpy(tags[i], argv[optind]);
+			tags_size++;
+			optind++;
+		}
+		optind = backup;
+	} else if (remaining_args > 0 && delete) {
+		int i;
+		tags = malloc(remaining_args * sizeof(*tags));
+		if (!tags) {
+			ret = -ENOMEM;
+			goto fail;
+		}
+		for (i = 0; i < remaining_args; i++) {
+			tags[i] = calloc(strlen(argv[optind]), sizeof(char));
+			if (!tags[i]) {
+				ret = -ENOMEM;
+				goto fail;
+			}
+			strcpy(tags[i], argv[optind]);
+			tags_size++;
+			optind++;
+		}
+	}
+fail:
+	return ret;
 }
 
 int tagCmd_execute(void)
 {
-	int ret = 0, i, j;
+	int ret = 0, i, j, char_count;
 	sqlite3 *handle;
-	char *sql;
+	char *sql, input, input_buffer[MAXBUFFERSIZE];
 
 	ret = safeword_db_open(&handle);
 	if (ret)
 		goto fail;
 
-	if (tags) {
+	if (tags && delete) {
+		for (i = 0; i < tags_size; i++) {
+			if (force) {
+				delete_tag(handle, tags[i]);
+				continue;
+			}
+			printf("delete tag '%s'? (y/N) ", tags[i]);
+			char_count = 0;
+			input = getchar();
+			while ((input != '\n') && (char_count < MAXBUFFERSIZE)) {
+				input_buffer[char_count++] = input;
+				input = getchar();
+			}
+			input_buffer[char_count] = 0x00;
+
+			/* skip comparing if just enter was pressed */
+			if (!char_count)
+				continue;
+
+			if (!strncasecmp("yes", input_buffer, char_count)) {
+				delete_tag(handle, tags[i]);
+			} else if (!strncasecmp("quit", input_buffer, char_count) ||
+				!strncasecmp("q", input_buffer, char_count))
+				break;
+		}
+	} else if (tags && credential_ids) {
 		for (i = 0; i < credential_ids_size; i++) {
 			for (j = 0; j < tags_size; j++) {
 				map_tag(handle, tags[j], credential_ids[i]);
