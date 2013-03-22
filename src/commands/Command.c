11129fbaae88cd0024a142c52199d30a90a847ef
Erich Schroeter <erichschroeter@gmail.com>
2013-03-21 19:13:18 -0500

    Added cp command to supported commands.

--- src/commands/Command.c
+++ src/commands/Command.c
@@ -4,6 +4,7 @@
 #include "AddCommand.h"
 #include "ListCommand.h"
 #include "TagCommand.h"
+#include "CopyCommand.h"
 
 struct command command_table[] = {
 	{"init", initCmd_help, initCmd_parse, initCmd_execute},
@@ -11,6 +12,7 @@ struct command command_table[] = {
 	{"add", addCmd_help, addCmd_parse, addCmd_execute},
 	{"ls", listCmd_help, listCmd_parse, listCmd_execute},
 	{"tag", tagCmd_help, tagCmd_parse, tagCmd_execute},
+	{"cp", copyCmd_help, copyCmd_parse, copyCmd_execute},
 };
 const size_t command_table_size = sizeof(command_table) / sizeof(command_table[0]);
 
