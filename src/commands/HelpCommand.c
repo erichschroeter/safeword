e66471249566cb0ebacda9167687bb6c97dc905a
Erich Schroeter <erichschroeter@gmail.com>
2013-09-15 18:25:15 -0500

    Implemented help command to use man pages and optionally html to a hard-coded url to github asciidoc pages.

--- src/commands/HelpCommand.c
+++ src/commands/HelpCommand.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 #include <string.h>
 
 #include "Command.h"
 #include "HelpCommand.h"
 
-static int command_index = -1;
+enum help_format {
+	HELP_FORMAT_NONE,
+	HELP_FORMAT_MAN,
+	HELP_FORMAT_HTML
+};
 
 char* helpCmd_help(void)
 {
@@ -15,33 +21,62 @@ char* helpCmd_help(void)
 "\n";
 }
 
-int helpCmd_parse(int argc, char** argv)
+int helpCmd_run(int argc, char** argv)
 {
-	int i, ret = 0;
+	int i, c, command_index = -1;
+	enum help_format format = HELP_FORMAT_NONE;
+	struct option long_options[] = {
+		{"man",  no_argument, NULL, 'm'},
+		{"html", no_argument, NULL, 'h'},
+	};
+	char *helpcmd = NULL;
 
-	if (argc < 2) {
+	while ((c = getopt_long(argc, argv, "mh", long_options, 0)) != -1) {
+		switch (c) {
+		case 'm':
+			format = HELP_FORMAT_MAN;
+			break;
+		case 'h':
+			format = HELP_FORMAT_HTML;
+			break;
+		}
+	}
+
+	/* We need a command to show help for. */
+	if (argc - optind < 1) {
 		printf("%s", helpCmd_help());
 		goto fail;
 	}
 
+	helpcmd = argv[optind];
+
+	/* Find the command to show help for. */
 	for (i = 0; i < command_table_size; i++) {
-		if (!strncmp(command_table[i].name, argv[1], strlen(argv[1]))) {
+		if (!strncmp(command_table[i].name, helpcmd, strlen(helpcmd))) {
 			command_index = i;
 		}
 	}
 
-fail:
-	return ret;
-}
+	if (command_index < 0) {
+		fprintf(stderr, "no help for '%s' command.\n", helpcmd);
+		goto fail;
+	}
 
-int helpCmd_run(int argc, char** argv)
-{
-	int ret = helpCmd_parse(argc, argv);
-	if (ret != 0) return ret;
-
-	/* TODO implement opening man page or html */
-	/*if (command_index >= 0)*/
-		/*printf("%s", command_table[command_index].help());*/
+	char cmd[256];
+	cmd[0] = 0;
+	switch (format) {
+	case HELP_FORMAT_HTML:
+		sprintf(cmd, "google-chrome https://raw.github.com/erichschroeter/safeword/master/doc/asciidoc/safeword-%s.txt", command_table[command_index].name);
+		break;
+	case HELP_FORMAT_MAN:
+	case HELP_FORMAT_NONE:
+	default:
+		sprintf(cmd, "man safeword-%s", command_table[command_index].name);
+		break;
+	}
+	system(cmd);
 
 	return 0;
+fail:
+	return -1;
 }
