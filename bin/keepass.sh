005b3ebf76b7e8ff2cff91a969c5149694d694a7
Erich Schroeter <erichschroeter@gmail.com>
2013-09-19 18:55:26 -0500

    Updated keepass import script; added support for note field. Added dependency on dos2unix and csvtool programs.

--- bin/keepass.sh
+++ bin/keepass.sh
@@ -2,6 +2,22 @@
 #
 #
 
+# exit immediately after fail
+set -e
+
+# Check for dependencies so we can parse the exported Keepass 1.x CSV
+hash csvtool 2>/dev/null ||
+hash dos2unix 2>/dev/null ||
+{ echo "
+This script depends on the following commands:
+
+  dos2unix
+  csvtool
+
+Please install via your package manager.
+"; exit 1;
+}
+
 usage()
 {
 	echo "USAGE: $0 [INPUT]
@@ -17,34 +33,35 @@ OPTIONS:
 
 convert_csv()
 {
-	IFS=","
-	count=`cat $1 | cut -d, -f1-4 | sed '/^$/d' | sed '/^"/!d' | sed '/^["]\s$/d' | wc -l`
-	count=$((count-1))
-	i=0
+	local CSV i count username password desc note
 
-	# 1. cut the comments field
-	# 2. filter out empty lines
-	# 3. filter out lines that don't start with quote
-	# 4. filter out lines with single quote and any remaining whitespace
-	cat $1 | cut -d, -f1-4 | sed '/^$/d' | sed '/^"/!d' | sed '/^["]\s$/d' | while read title username password url
-	do
-		echo "{"
-		echo -e "\t\"message\": $title,"
-		echo -e "\t\"username\": $username,"
-		echo -e "\t\"password\": $password,"
-		echo -e "\t\"tags\": ["
-		echo -e "\t\t\"\""
-		echo -e "\t]"
-		# don't print the comma on the last one
-		[ $i -eq $count ] && echo "}" || echo "},"
-		i=$((i+1))
+	CSV=$1
+	[ -z "$CSV" ] && { echo "no CSV file specified"; exit 1; }
+
+	dos2unix $CSV
+
+	i=1
+	count=`csvtool height $CSV`
+	echo "importing $count credentials"
+	while [ $i -lt $count ]; do
+		username=`csvtool drop $i $CSV | csvtool take 1 - | csvtool col 2 -`
+		password=`csvtool drop $i $CSV | csvtool take 1 - | csvtool col 3 -`
+		desc=`csvtool drop $i $CSV | csvtool take 1 - | csvtool col 1 -`
+		# Uncomment below to replace all newlines with '\n'.
+		# NOTE: You need to comment the line after so you don't add duplicates.
+		#note=`csvtool drop $i $CSV | csvtool take 1 - | csvtool col 5 - | sed ':a;N;$!ba;s/\n/\\\\n/g'`
+		note=`csvtool drop $i $CSV | csvtool take 1 - | csvtool col 5 -`
+		#echo -e "safeword -m '$desc' -n '$note' '$username' '$password'"
+		safeword add -m "$desc" -n "$note" "$username" "$password"
+
+		let i=i+1
 	done
 }
 
 case $1 in
 csv)
-	[ $# -gt 1 ] && [ -f $2 ] || exit 1
-	convert_csv $2
+	shift
+	convert_csv $1
 	;;
 *)
 	usage
