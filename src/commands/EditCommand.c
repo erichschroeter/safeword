e470ea0578c300f264f8c842812da3b7e7f9567d
Erich Schroeter <erichschroeter@gmail.com>
2013-09-19 16:39:15 -0500

    Updated edit command to support editing note field.

--- src/commands/EditCommand.c
+++ src/commands/EditCommand.c
@@ -10,56 +10,121 @@
 #include "EditCommand.h"
 #include "dbg.h"
 
-static int _credential_id;
-static char *_username = 0;
-static char *_password = 0;
-static char *_message = 0;
-
-char* editCmd_help(void)
+static int read_file_to_string(char *file, char **strptr)
 {
-	return "SYNOPSIS\n"
-"	edit [-uUSERNAME | --username=USERNAME] [-pPASSWORD | --password=PASSWORD]\n"
-"		[-mMESSAGE | --message=MESSAGE] ID\n"
-"\n"
-"DESCRIPTION\n"
-"	The edit command edits credential information. With no options the credential information is\n"
-"	opened in the default text editor to manually edit.\n"
-"\n"
-"OPTIONS\n"
-"	-u, --username\n"
-"	    Edit the username.\n"
-"	-p, --password\n"
-"	    Edit the password.\n"
-"	-m, --message\n"
-"	    Edit the message.\n"
-"\n";
+	int ret = 0;
+	long file_size = 0;
+	FILE *fd = NULL;
+
+	/* Special case: use stdin if file is hyphen "-". */
+	if (!strcmp(file, "-")) {
+		fd = stdin;
+	} else {
+		fd = fopen(file, "r");
+		if (fd == NULL) return errno;
+	}
+
+	if (fd == stdin) {
+		/* Read from standard input into a string. */
+		char buffer[1024];
+		buffer[0] = '\0';
+		file_size = 0;
+
+		*strptr = calloc(1024, sizeof(char));
+		safeword_check(*strptr, -ENOMEM, fail);
+
+		while (fgets(buffer, 1024, fd)) {
+			char *old = *strptr;
+			file_size += strlen(buffer);
+			*strptr = realloc(*strptr, file_size + 1);
+			if (!*strptr) {
+				ret = -ENOMEM;
+				free(old);
+				goto fail;
+			}
+			strcat(*strptr, buffer);
+		}
+	} else {
+		/* Read from specified file into a string. */
+		ret = fseek(fd, 0, SEEK_END);
+		safeword_check(ret != -1, errno, fail);
+
+		file_size = ftell(fd);
+		safeword_check(file_size != -1, errno, fail);
+
+		ret = fseek(fd, 0, SEEK_SET);
+		safeword_check(ret != -1, errno, fail);
+
+		*strptr = calloc(file_size + 1, sizeof(char));
+		safeword_check(*strptr, -ENOMEM, fail);
+
+		ret = fread(*strptr, 1, file_size, fd);
+		safeword_check(ret == file_size, errno, fail);
+	}
+
+	return 0;
+fail:
+	free(*strptr);
+	return -1;
 }
 
-int editCmd_parse(int argc, char** argv)
+static int editCmd_parse(int argc, char** argv, int *credential, char **username, char **password,
+	char **desc, char **note)
 {
 	int ret = 0, c;
+	FILE *notefile = NULL;
 	struct option long_options[] = {
 		{"username", required_argument, NULL, 'u'},
 		{"password", required_argument, NULL, 'p'},
 		{"message",  required_argument, NULL, 'm'},
+		{"note",     required_argument, NULL, 'n'},
 	};
 
-	while ((c = getopt_long(argc, argv, "u:p:m:", long_options, 0)) != -1) {
+	while ((c = getopt_long(argc, argv, "u:p:m:n:", long_options, 0)) != -1) {
 		switch (c) {
 		case 'u':
-			_username = calloc(strlen(optarg) + 1, sizeof(char));
-			safeword_check(_username, -ENOMEM, fail);
-			_username = strcpy(_username, optarg);
+			*username = calloc(strlen(optarg) + 1, sizeof(char));
+			safeword_check(*username, -ENOMEM, fail);
+			*username = strcpy(*username, optarg);
 			break;
 		case 'p':
-			_password = calloc(strlen(optarg) + 1, sizeof(char));
-			safeword_check(_password, -ENOMEM, fail);
-			_password = strcpy(_password, optarg);
+			*password = calloc(strlen(optarg) + 1, sizeof(char));
+			safeword_check(*password, -ENOMEM, fail);
+			*password = strcpy(*password, optarg);
 			break;
 		case 'm':
-			_message = calloc(strlen(optarg) + 1, sizeof(char));
-			safeword_check(_message, -ENOMEM, fail);
-			_message = strcpy(_message, optarg);
+			*desc = calloc(strlen(optarg) + 1, sizeof(char));
+			safeword_check(*desc, -ENOMEM, fail);
+			*desc = strcpy(*desc, optarg);
+			break;
+		case 'n':
+			if (!strncmp(optarg, "-", 1)) {
+				ret = read_file_to_string(optarg, note);
+				safeword_check(ret == 0, errno, fail);
+			} else {
+				notefile = fopen(optarg, "r");
+				if (notefile == NULL) {
+					/*
+					 * Assume if the file doesn't exist that a
+					 * string was specified.
+					 */
+					switch (errno) {
+					case EISDIR:
+					case EACCES:
+						return 3;
+					case ENOENT:
+					default:
+						*note = calloc(strlen(optarg) + 1, sizeof(char));
+						safeword_check(*note != NULL, -ENOMEM, fail);
+						strcpy(*note, optarg);
+						break;
+					}
+				} else {
+					fclose(notefile);
+					ret = read_file_to_string(optarg, note);
+					safeword_check(ret == 0, errno, fail);
+				}
+			}
 			break;
 		}
 	}
@@ -71,35 +136,38 @@ int editCmd_parse(int argc, char** argv)
 		goto fail;
 	}
 
-	_credential_id = strtol(argv[optind], NULL, 10);
+	errno = 0;
+	*credential = strtol(argv[optind], NULL, 10);
 	if (errno) {
 		fprintf(stderr, "failed to parse ID: %s\n", strerror(errno));
 		goto fail;
 	}
 
-	return ret;
+	return 0;
 
 fail:
-	free(_username);
-	free(_password);
-	free(_message);
-	_credential_id = 0; /* set to invalid id */
+	free(*username);
+	free(*password);
+	free(*desc);
+	free(*note);
+	credential = 0; /* set to invalid id */
 
-	return ret;
+	return -1;
 }
 
 int editCmd_run(int argc, char** argv)
 {
-	int ret;
+	int ret, credential;
 	struct safeword_db db;
 	struct safeword_credential cred;
+	char *username = NULL, *password = NULL, *desc = NULL, *note = NULL;
 
-	ret = editCmd_parse(argc, argv);
+	ret = editCmd_parse(argc, argv, &credential, &username, &password, &desc, &note);
 	if (ret != 0) return ret;
 
 	memset(&cred, 0, sizeof(cred));
 
-	if (!_credential_id) {
+	if (!credential) {
 		ret = -ESAFEWORD_ILLEGALARG;
 		goto fail;
 	}
@@ -132,10 +200,11 @@ int editCmd_run(int argc, char** argv)
 	}
 #endif
 
-	cred.id = _credential_id;
-	cred.username = _username;
-	cred.password = _password;
-	cred.description = _message;
+	cred.id = credential;
+	cred.username = username;
+	cred.password = password;
+	cred.description = desc;
+	cred.note = note;
 
 	ret = safeword_credential_update(&db, &cred);
 	safeword_check(!ret, ret, fail);
@@ -143,9 +212,10 @@ int editCmd_run(int argc, char** argv)
 	safeword_close(&db);
 
 fail:
-	free(_username);
-	free(_password);
-	free(_message);
+	free(username);
+	free(password);
+	free(desc);
+	free(note);
 
 	return ret;
 }
